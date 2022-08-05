67d34e7b59b8c12a7abb8036e33491ab32e5fd5b
Daniel Andrews <daniel.korienko@gmail.com>
2022-08-05 18:39:53 +0300

    Test commit

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
 import "./Header.css";
+import Icon from "../../assets/logo.svg";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header-content w-1360">
-          <div className="header-content__logo">
-              
+        <div className="header-content__logo-text">
+          <img src={Icon} alt="logo" />
+          <div>
+            <h2>Pizza Kyiv</h2>
+            <p>№1 Pizza App in Ukraine</p>
           </div>
+        </div>
       </div>
     </div>
   );
