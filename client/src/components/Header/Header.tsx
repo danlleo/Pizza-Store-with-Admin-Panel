e1b351d98dc572948f3235def0070862f6b5f20c
Daniel Andrews <daniel.korienko@gmail.com>
2022-08-05 18:15:50 +0300

    Test commit

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content w-1360">
+          <div className="header-content__logo">
+              
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
