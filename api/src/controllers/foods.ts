b6335677722a12c4872f58420382e80b463747a1
Mark Norton <us.marknorton@gmail.com>
2022-08-07 07:12:28 +0300

    added patch and delete images

--- api/src/controllers/foods.ts
+++ api/src/controllers/foods.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from 'express'
 import path from 'path'
 import { v4 as uuidv4 } from 'uuid'
-import Foods from '../models/foods';
+import { unlinkSync } from 'fs'
+import Foods from '../models/foods'
+
 
 const MESSAGE_DELETED = 'Deleted Successfully.'
+const FOLDER_PATH = path.join(__dirname, '..', 'public', 'foods')
+const FOOD_IMAGES_PATH = path.join(FOLDER_PATH, 'images')
 
 export async function getAllFoodController(req: Request, res: Response){
     try {
@@ -39,25 +43,30 @@ export async function createFoodController(req: any, res: any){
 
     try {
         const newFood = await food.save()
-        image.mv(path.resolve(__dirname, '..', 'public', 'images', fileName))
+        image.mv(path.resolve(FOOD_IMAGES_PATH, fileName))
         res.status(201).json(newFood)
     } catch (e: any) {
         res.status(400).json({ message: e.message })
     }
 }
 
-export async function patchFoodController(req: Request, res: any){
-    const {name, title, cost, type, calories, fat, sugar, salt, image_path} = req.body
+export async function patchFoodController(req: any, res: any){
+    const {name, title, cost, type, calories, fat, sugar, salt} = req.body
+    const {image} = req.files
 
     if (name !== null || title !== null) res.food.name = name || title
     if (cost !== null) res.food.cost = cost
     if (type !== null) res.food.type = type
-    if (image_path !== null) res.food.image_path = image_path
     if (calories !== null) res.food.calories = calories
     if (fat !== null) res.food.fat = fat
     if (sugar !== null) res.food.sugar = sugar
     if (salt !== null) res.food.salt = salt
-
+    if (image !== null) {
+        const fileName = `${uuidv4()}${path.extname(image.name)}`
+        unlinkSync(path.resolve(FOOD_IMAGES_PATH, res.food.image_path))
+        res.food.image_path = fileName
+        image.mv(path.resolve(FOOD_IMAGES_PATH, fileName))
+    }
     try {
         const updatedFood = await res.food.save()
         res.status(201).json(updatedFood)
@@ -69,6 +78,7 @@ export async function patchFoodController(req: Request, res: any){
 export async function deleteFoodController(req: Request, res: any){
     try {
         await res.food.remove()
+        unlinkSync(path.resolve(FOOD_IMAGES_PATH, res.food.image_path))
         res.json({ message: MESSAGE_DELETED })
     } catch (e: any) {
         res.status(500).json({ message: e.message })
