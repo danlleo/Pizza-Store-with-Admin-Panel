78517336acd55972ed0e2ead12c37b80e6926348
Mark Norton <us.marknorton@gmail.com>
2022-08-07 12:13:26 +0300

    switched name to type in foodtypes

--- api/src/models/foodtypes.ts
+++ api/src/models/foodtypes.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const FoodTypesSchema = new Schema({
-    name: { type: String, required: true, unique: true }
+    type: { type: String, required: true, unique: true }
 })
 
 const FoodTypes = mongoose.model('FoodTypes', FoodTypesSchema)
