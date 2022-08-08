035317f01df62be42ecb174eb36c85d69030aaed
Daniel Andrews <daniel.korienko@gmail.com>
2022-08-08 13:17:44 +0300

    Interface update

--- client/src/components/DishItem/DishItem.tsx
+++ client/src/components/DishItem/DishItem.tsx
@@ -5,19 +5,22 @@ const DishItem = () => {
     <div className="dishItem">
       <div className="dishItem-image">
         <img
-          src="https://assets.dots.live/misteram-public/ca9178c1bce556857ad5dc6128fc2a5c-826x0.png"
+          src="https://cdn.dodostatic.net/static/Img/Products/5630c6ed3f394c7ba25e1ef79a67b7ee_292x292.jpeg"
           alt="pizza"
         />
       </div>
       <div className="dishItem-about">
         <div className="dishItem-about__name">
-          <p>Маргаріта, розмір L</p>
-        </div>
-        <div className="dishItem-about__info">
-          <p className="dishItem-about__info-price">175 ₴</p>
-          <p>500 G</p>
+          <p style={{ fontSize: "20px" }}>Ham & Cheese</p>
+          <p style={{ fontSize: "12px", color: "#5C6370" }}>
+            Ham, Mozzarella, Alfredo sauce
+          </p>
         </div>
       </div>
+        <div className="dishItem-about__info">
+          <p className="dishItem-about__info-price">10 $</p>
+          <button>Choose</button>
+        </div>
     </div>
   );
 };
