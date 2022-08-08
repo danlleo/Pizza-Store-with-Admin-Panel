ca3561396802a722f2225cebfbd744e9095b815c
Daniel Andrews <daniel.korienko@gmail.com>
2022-08-08 20:24:32 +0300

    Small interface update

--- client/src/components/Banner/Banner.tsx
+++ client/src/components/Banner/Banner.tsx
@@ -2,8 +2,10 @@ import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div className="banner w-1560" style={{ margin: "2rem auto 1rem" }}>
-      <h1>Best Pizza App in NYC</h1>
+    <div className="clap-width">
+      <div className="banner" style={{ margin: "2rem auto 1rem" }}>
+        <h1>Best Pizza App in NYC</h1>
+      </div>
     </div>
   );
 };
