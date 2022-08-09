87aee286456f03b31f92f956e21dc424f3021d56
Mark Norton <us.marknorton@gmail.com>
2022-08-09 10:19:42 +0300

    Revert "Revert "Revert "Merge remote-tracking branch 'origin/main' into main"""
    This reverts commit a9723b89a1a911a1b017a498a2bfe0b23043661c.

--- client/src/components/DishContainer/DishContainer.tsx
+++ client/src/components/DishContainer/DishContainer.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import DishItem from "../DishItem/DishItem"
-import "./DishContainer.css"
-=======
 import DishItem from '../DishItem/DishItem'
 import './DishContainer.css'
->>>>>>> 4f40845c6499c61f67e9f42aafe2bd3be62ee510
 
 interface Props {
   container_id: string
@@ -13,9 +8,9 @@ interface Props {
 
 const DishContainer = ({ container_id, data }: Props) => {
   return (
-    <div className='dishContainer clap-width' id={container_id}>
+    <div className="dishContainer clap-width" id={container_id}>
       <h1>Pizza</h1>
-      <div className='dishContainer-list'>
+      <div className="dishContainer-list">
         <DishItem />
         <DishItem />
         <DishItem />
