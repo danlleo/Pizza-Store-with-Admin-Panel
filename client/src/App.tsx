825bb8a9768367ec07a52102192f150250398cbe
Daniel Andrews <daniel.korienko@gmail.com>
2022-08-10 18:54:41 +0300

    Adding Redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,16 @@ import Header from './components/Header/Header'
 import Cart from './components/Cart/Cart'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
+  const [show, setShow] = useState(true);
+
   return (
     <>
       <Header />
-      <Cart />
+      {show && <Cart />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
