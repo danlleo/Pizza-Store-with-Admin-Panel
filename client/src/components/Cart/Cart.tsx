ebc3a632298fece293d630e6dceeced810790ed3
Mark Norton <us.marknorton@gmail.com>
2022-08-10 19:36:57 +0300

    cart close button styles

--- client/src/components/Cart/Cart.tsx
+++ client/src/components/Cart/Cart.tsx
@@ -6,16 +6,18 @@ const Cart = () => {
       <div className='cart'>
         <div className='cart__background'></div>
         <div className='cart__container'>
-          <button className='cart__container-close'>
-            <svg viewBox='0 0 24 24' fill='none'>
-              <path
-                fillRule='evenodd'
-                clipRule='evenodd'
-                d='M9.61 12.199L.54 3.129A1.833 1.833 0 113.13.536l9.07 9.07L21.27.54a1.833 1.833 0 012.592 2.592l-9.068 9.068 9.07 9.07a1.833 1.833 0 01-2.59 2.592l-9.072-9.07-9.073 9.073a1.833 1.833 0 01-2.591-2.592L9.61 12.2z'
-                fill='#fff'
-              ></path>
-            </svg>
-          </button>
+          <div className='cart__container-area'>
+            <button className='cart__container-close'>
+              <svg viewBox='0 0 24 24' fill='none'>
+                <path
+                  fillRule='evenodd'
+                  clipRule='evenodd'
+                  d='M9.61 12.199L.54 3.129A1.833 1.833 0 113.13.536l9.07 9.07L21.27.54a1.833 1.833 0 012.592 2.592l-9.068 9.068 9.07 9.07a1.833 1.833 0 01-2.59 2.592l-9.072-9.07-9.073 9.073a1.833 1.833 0 01-2.591-2.592L9.61 12.2z'
+                  fill='#fff'
+                ></path>
+              </svg>
+            </button>
+          </div>
           <div className='cart__container-about'>
             <img
               src='https://cdn.dodostatic.net/site-static/dist/121df529925b0f43cc73.svg'
