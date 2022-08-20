acd01cde3dd1b6eb59cc5766f8ef8af8efbbc160
Daniel Andrews <daniel.korienko@gmail.com>
2022-08-20 14:04:39 +0300

    Fixed client bugs

--- clients/pizza-store/.eslintrc.js
+++ clients/pizza-store/.eslintrc.js
@@ -1,34 +1,30 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'standard',
-      'prettier'
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module'
-    },
-    plugins: [
-      'react',
-      '@typescript-eslint'
-    ],
-    rules: {
-      'import/no-anonymous-default-export': 0,
-      'react/jsx-uses-react': 0,
-      'react/display-name': 0,
-      'react/react-in-jsx-scope': 0,
-      'no-unused-vars': 1
-    }
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/no-anonymous-default-export': 0,
+    'react/jsx-uses-react': 0,
+    'react/display-name': 0,
+    'react/react-in-jsx-scope': 0,
+    'no-unused-vars': 1
   }
-  
+}
--- clients/pizza-store/src/store/index.ts
+++ clients/pizza-store/src/store/index.ts
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-// import displayReducer from './displaySlice';
+// import { configureStore } from '@reduxjs/toolkit'
+// // import displayReducer from './displaySlice';
 
-const store = configureStore({
-  reducer: {}
-})
+// const store = configureStore({
+//   reducer: {}
+// })
 
-export default store
+// export default store
 
-export type RootState = ReturnType<typeof store.getState>
+// export type RootState = ReturnType<typeof store.getState>
