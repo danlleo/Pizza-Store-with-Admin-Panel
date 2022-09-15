026e248078c43896ac5baadd81b3926c8e5ef5fb
Daniel Andrews <daniel.korienko@gmail.com>
2022-09-15 14:28:36 +0300

    UI update

---
+++ clients/admin-panel/src/components/table/Table.tsx
@@ -0,0 +1,105 @@
+import './Table.css'
+
+const Table = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Category</th>
+          <th>Status</th>
+          <th>Price</th>
+          <th>Portion size</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Pepperoni</td>
+          <td>Pizza</td>
+          <td>Pending</td>
+          <td>10$</td>
+          <td>Small</td>
+        </tr>
+        <tr>
+          <td>Margarita</td>
+          <td>Pizza</td>
+          <td>Cooking</td>
+          <td>12$</td>
+          <td>Medium</td>
+        </tr>
+        <tr>
+          <td>Pasta</td>
+          <td>Main serving</td>
+          <td>Delivered</td>
+          <td>7$</td>
+          <td>Small</td>
+        </tr>
+        <tr>
+          <td>Sushi</td>
+          <td>Main serving</td>
+          <td>Delivering</td>
+          <td>15$</td>
+          <td>Large</td>
+        </tr>
+        <tr>
+          <td>Pepperoni</td>
+          <td>Pizza</td>
+          <td>Pending</td>
+          <td>10$</td>
+          <td>Small</td>
+        </tr>
+        <tr>
+          <td>Margarita</td>
+          <td>Pizza</td>
+          <td>Cooking</td>
+          <td>12$</td>
+          <td>Medium</td>
+        </tr>
+        <tr>
+          <td>Pasta</td>
+          <td>Main serving</td>
+          <td>Delivered</td>
+          <td>7$</td>
+          <td>Small</td>
+        </tr>
+        <tr>
+          <td>Sushi</td>
+          <td>Main serving</td>
+          <td>Delivering</td>
+          <td>15$</td>
+          <td>Large</td>
+        </tr>
+        <tr>
+          <td>Pepperoni</td>
+          <td>Pizza</td>
+          <td>Pending</td>
+          <td>10$</td>
+          <td>Small</td>
+        </tr>
+        <tr>
+          <td>Margarita</td>
+          <td>Pizza</td>
+          <td>Cooking</td>
+          <td>12$</td>
+          <td>Medium</td>
+        </tr>
+        <tr>
+          <td>Pasta</td>
+          <td>Main serving</td>
+          <td>Delivered</td>
+          <td>7$</td>
+          <td>Small</td>
+        </tr>
+        <tr>
+          <td>Sushi</td>
+          <td>Main serving</td>
+          <td>Delivering</td>
+          <td>15$</td>
+          <td>Large</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default Table
--- clients/admin-panel/src/pages/home/Home.tsx
+++ clients/admin-panel/src/pages/home/Home.tsx
@@ -1,11 +1,65 @@
 import Navbar from '../../layout/navbar/Navbar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faDollar,
+  faBars,
+  faSheetPlastic,
+  faBellConcierge
+} from '@fortawesome/free-solid-svg-icons'
 import './Home.css'
+import Table from '../../components/table/Table'
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
-      <div className='home__container'></div>
+      <div className='home__body'>
+        <input type='text' placeholder='Search item..' />
+        <div className='home__body__blocks'>
+          <div className='home__body__blocks__item'>
+            <p>Total items</p>
+            <FontAwesomeIcon
+              icon={faBars}
+              style={{
+                color: '#82B6F2'
+              }}
+            />
+            <h1>40,876</h1>
+          </div>
+          <div className='home__body__blocks__item'>
+            <p>Total categories</p>
+            <FontAwesomeIcon
+              icon={faSheetPlastic}
+              style={{
+                color: '#39C586'
+              }}
+            />
+            <h1>40,876</h1>
+          </div>
+          <div className='home__body__blocks__item'>
+            <p>Total orderds</p>
+            <FontAwesomeIcon
+              icon={faBellConcierge}
+              style={{
+                color: '#FCC372'
+              }}
+            />
+            <h1>40,876</h1>
+          </div>
+          <div className='home__body__blocks__item'>
+            <p>Total revenue</p>
+            <FontAwesomeIcon
+              icon={faDollar}
+              style={{
+                color: '#E0858A'
+              }}
+            />
+            <h1>40,876</h1>
+          </div>
+        </div>
+        <h1 style={{ margin: '2.5rem 0 1rem' }}>Recent Sales</h1>
+        <Table />
+      </div>
     </div>
   )
 }
