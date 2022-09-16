d4c88faf7e1f9105fa83bac5dd0767c073f9161b
Daniel Andrews <daniel.korienko@gmail.com>
2022-09-16 20:18:01 +0300

    Header update

--- clients/admin-panel/src/layout/navbar/Navbar.tsx
+++ clients/admin-panel/src/layout/navbar/Navbar.tsx
@@ -13,11 +13,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='navbar__logo'>
-        <img
-          src='https://images.squarespace-cdn.com/content/v1/59ea6080a803bb2f70ecbae5/1545587567641-ZWHZLLT6TEFLHGZZINON/3ea3f2a0fd.c40c11dd0a.MhId2s.png?format=500w'
-          alt='logo-pic'
-        />
-        <h2>WS ADMIN</h2>
+        <h2>ADMIN PANEL</h2>
       </div>
       <div className='navbar__links'>
         <Link to='/'>
