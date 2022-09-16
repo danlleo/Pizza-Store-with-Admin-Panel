15e62f3b0ccc0a5e36234bdabe3b97df53b72e5f
Daniel Andrews <daniel.korienko@gmail.com>
2022-09-16 20:50:16 +0300

    Color update

--- clients/admin-panel/src/pages/products/Products.tsx
+++ clients/admin-panel/src/pages/products/Products.tsx
@@ -1,3 +1,4 @@
+import Product from '../../components/product/Product'
 import Navbar from '../../layout/navbar/Navbar'
 import './Products.css'
 
@@ -5,7 +6,17 @@ const Products = () => {
   return (
     <div className='products'>
       <Navbar />
-      <div className="products__body"></div>
+      <div className='products__body'>
+        <div className='products__body__head'>
+          <h1>All Products</h1>
+          <button>Add Product</button>
+        </div>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
     </div>
   )
 }
